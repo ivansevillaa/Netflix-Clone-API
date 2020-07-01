e423d57ed49509895d42cb7cf54f10edd28f2ad4
Ivan Sevilla <ivansevillaa@gmail.com>
2020-07-01 16:02:08 -0300

    added new config and env variables

--- config/index.js
+++ config/index.js
@@ -3,4 +3,9 @@ require('dotenv').config();
 module.exports = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 3000,
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
 };
