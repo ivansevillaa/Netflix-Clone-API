946378be42e42363bc356061ac6ced88751a8ff0
Ivan Sevilla <ivansevillaa@gmail.com>
2020-07-01 16:03:10 -0300

    added lib layer for database and models

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const config = require('./config/index');
 const moviesApi = require('./routes/movies');
+const mongo = require('./lib/mongo');
 
 const app = express();
 
+// body parser
+app.use(express.json());
+
 moviesApi(app);
+mongo();
 
 app.listen(config.port, () => {
   console.log(`App is listening on http://localhost:${config.port}`);
