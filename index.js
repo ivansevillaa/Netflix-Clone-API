650f8fc3b4fed1a6e977439c9e69bab226826cfe
Ivan Sevilla <ivansevillaa@gmail.com>
2020-07-05 14:28:21 -0300

    added 404 error handler

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const express = require('express');
 const config = require('./config/index');
 const moviesApi = require('./routes/movies');
 const mongo = require('./lib/mongo');
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const {
+  logErrors,
+  errorHandler,
+  wrapErrors,
+} = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 const app = express();
 
+// connect to moongodb
 mongo();
 
 // body parser
 app.use(express.json());
+
 // routes
 moviesApi(app);
+
+// 404 handler
+app.use(notFoundHandler);
+
 // error handlers
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
--- utils/middleware/errorHandlers.js
+++ utils/middleware/errorHandlers.js
@@ -1,9 +1,10 @@
+const boom = require('@hapi/boom');
 const config = require('../../config/index');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
     return {
-      error,
+      ...error,
       stack,
     };
   }
@@ -17,11 +18,24 @@ function logErrors(err, req, res, next) {
   next(err);
 }
 
+function wrapErrors(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err));
+  }
+
+  next(err);
+}
+
 function errorHandler(err, req, res, next) {
-  res.status(err.status || 500).json(withErrorStack(err.message, err.stack));
+  const {
+    output: { statusCode, payload },
+  } = err;
+
+  res.status(statusCode || 500).json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
   errorHandler,
+  wrapErrors,
   logErrors,
 };
