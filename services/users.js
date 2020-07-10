56604a5d80ac544de7abae55ab8abb4366c070bc
Ivan Sevilla <ivansevillaa@gmail.com>
2020-07-10 19:27:23 -0300

    added service layer for users

---
+++ services/users.js
@@ -0,0 +1,72 @@
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+const Users = require('../lib/models/users');
+
+async function listUsers() {
+  const users = await Users.find();
+
+  return users;
+}
+
+async function getUser(email) {
+  const user = await Users.find({ email });
+  if (!user) {
+    throw boom.notFound('User not found');
+  }
+
+  return user;
+}
+
+async function createUser(user) {
+  const email = await Users.find({ email: user.email });
+  if (email) {
+    throw boom.conflict('Email already exists');
+  }
+
+  const username = await Users.find({ username: user.username });
+  if (username) {
+    throw boom.conflict('Username already exists');
+  }
+
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+
+  const newUser = new Users({
+    ...user,
+    password: hashedPassword,
+  });
+  const createdUser = await newUser.save();
+
+  return createdUser;
+}
+
+async function updateUser(userId, userData) {
+  const updatedUser = await Users.findOneAndUpdate(
+    { _id: userId },
+    { $set: userData },
+    { new: true }
+  );
+
+  if (!updatedUser) {
+    throw boom.notFound('User not found');
+  }
+
+  return updatedUser;
+}
+
+async function deleteUser(userId) {
+  const user = await Users.findById(userId);
+
+  if (user) {
+    await Users.deleteOne(user);
+  } else {
+    throw boom.notFound('User not found');
+  }
+}
+
+module.exports = {
+  listUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
