572efe37080ff81c19d79cda8aa552551303447f
Ivan Sevilla <ivansevillaa@gmail.com>
2020-07-10 19:29:13 -0300

    added model and schemas for users

---
+++ utils/schemas/users.js
@@ -0,0 +1,34 @@
+const joi = require('@hapi/joi');
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// Minimum eight characters, at least one uppercase letter, one lowercase letter and one numbe
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{8,}$/;
+
+const userIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/); // mongodb id regex
+const userEmailSchema = joi.string().max(99).regex(emailRegex);
+const userUsernameSchema = joi.string().min(1).max(16);
+const userPasswordSchema = joi.string().regex(passwordRegex);
+const userPhotoSrcSchema = joi.string().uri();
+const userIsAdminSchema = joi.boolean();
+
+const createUserSchema = {
+  email: userEmailSchema.required(),
+  username: userUsernameSchema.required(),
+  password: userPasswordSchema.required(),
+  photo_src: userPhotoSrcSchema,
+  isAdmin: userIsAdminSchema,
+};
+
+const updateUserSchema = {
+  email: userEmailSchema,
+  username: userUsernameSchema,
+  password: userPasswordSchema,
+  photo_src: userPhotoSrcSchema,
+  isAdmin: userIsAdminSchema,
+};
+
+module.exports = {
+  userIdSchema,
+  createUserSchema,
+  updateUserSchema,
+};
