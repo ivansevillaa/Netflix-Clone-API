35f48f75510f35e7accced84be15b5b430ca70c9
Ivan Sevilla <ivansevillaa@gmail.com>
2020-07-11 13:41:33 -0300

    modularized code in routes

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable comma-dangle */
 const express = require('express');
 const genreService = require('../services/genres');
@@ -8,97 +9,107 @@ function genresApi(app) {
   const router = express.Router();
   app.use('/genres', router);
 
-  router.get('/', async (req, res, next) => {
-    try {
-      const genres = await genreService.listGenres();
-
-      res.status(200).send({
-        error: false,
-        message: 'Genres listed succesfully',
-        data: genres,
-      });
-    } catch (err) {
-      next(err);
-    }
-  });
+  router.get('/', listMovies);
 
   router.get(
     '/:genreId',
     validationHandler({ genreId: genreIdSchema }, 'params'),
-    async (req, res, next) => {
-      const { genreId } = req.params;
-
-      try {
-        const genre = await genreService.getGenre(genreId);
-
-        res.status(200).send({
-          error: false,
-          message: 'Genre retrieved succesfully',
-          data: genre,
-        });
-      } catch (err) {
-        next(err);
-      }
-    }
+    getGenre
   );
 
-  router.post('/', validationHandler(genreSchema), async (req, res, next) => {
-    const { body: genre } = req;
-
-    try {
-      const createdGenre = await genreService.createGenre(genre);
-
-      res.status(201).send({
-        error: false,
-        message: 'Genre created succesfully',
-        data: createdGenre,
-      });
-    } catch (err) {
-      next(err);
-    }
-  });
+  router.post('/', validationHandler(genreSchema), createGenre);
 
   router.patch(
     '/:genreId',
     validationHandler({ genreId: genreIdSchema }, 'params'),
     validationHandler(genreSchema),
-    async (req, res, next) => {
-      const { genreId } = req.params;
-      const { body: genreData } = req;
-
-      try {
-        const updatedGenre = await genreService.updateGenre(genreId, genreData);
-
-        res.status(200).send({
-          error: false,
-          message: 'Genre updated succesfully',
-          data: updatedGenre,
-        });
-      } catch (err) {
-        next(err);
-      }
-    }
+    updateGenre
   );
 
   router.delete(
     '/:genreId',
     validationHandler({ genreId: genreIdSchema }, 'params'),
-    async (req, res, next) => {
-      const { genreId } = req.params;
-
-      try {
-        await genreService.deleteGenre(genreId);
-
-        res.status(200).send({
-          error: false,
-          message: 'Genre deleted succesfully',
-          data: {},
-        });
-      } catch (err) {
-        next(err);
-      }
-    }
+    deleteGenre
   );
+}
+
+async function listMovies(req, res, next) {
+  try {
+    const genres = await genreService.listGenres();
+
+    res.status(200).send({
+      error: false,
+      message: 'Genres listed succesfully',
+      data: genres,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getGenre(req, res, next) {
+  const { genreId } = req.params;
+
+  try {
+    const genre = await genreService.getGenre(genreId);
+
+    res.status(200).send({
+      error: false,
+      message: 'Genre retrieved succesfully',
+      data: genre,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function createGenre(req, res, next) {
+  const { body: genre } = req;
+
+  try {
+    const createdGenre = await genreService.createGenre(genre);
+
+    res.status(201).send({
+      error: false,
+      message: 'Genre created succesfully',
+      data: createdGenre,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateGenre(req, res, next) {
+  const { genreId } = req.params;
+  const { body: genreData } = req;
+
+  try {
+    const updatedGenre = await genreService.updateGenre(genreId, genreData);
+
+    res.status(200).send({
+      error: false,
+      message: 'Genre updated succesfully',
+      data: updatedGenre,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteGenre(req, res, next) {
+  const { genreId } = req.params;
+
+  try {
+    await genreService.deleteGenre(genreId);
+
+    res.status(200).send({
+      error: false,
+      message: 'Genre deleted succesfully',
+      data: {},
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = genresApi;
--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable comma-dangle */
 const express = require('express');
 const movieService = require('../services/movies');
@@ -12,103 +13,109 @@ function moviesApi(app) {
   const router = express.Router();
   app.use('/movies', router);
 
-  router.get('/', async (req, res, next) => {
-    const { genre } = req.query;
-
-    try {
-      const movies = await movieService.listMovies(genre);
-
-      res.status(200).send({
-        error: false,
-        message: 'Movies listed succesfully',
-        data: movies,
-      });
-    } catch (err) {
-      next(err);
-    }
-  });
+  router.get('/', listMovies);
 
   router.get(
     '/:movieId',
     validationHandler({ movieId: movieIdSchema }, 'params'),
-    async (req, res, next) => {
-      const { movieId } = req.params;
-
-      try {
-        const movie = await movieService.getMovie(movieId);
-
-        res.status(200).send({
-          error: false,
-          message: 'Movie retrieved succesfully',
-          data: movie,
-        });
-      } catch (err) {
-        next(err);
-      }
-    }
+    getMovie
   );
 
-  router.post(
-    '/',
-    validationHandler(createMovieSchema),
-    async (req, res, next) => {
-      const { body: movie } = req;
-
-      try {
-        const createdMovie = await movieService.createMovie(movie);
-
-        res.status(201).send({
-          error: false,
-          message: 'Movie created succesfully',
-          data: createdMovie,
-        });
-      } catch (err) {
-        next(err);
-      }
-    }
-  );
+  router.post('/', validationHandler(createMovieSchema), createMovie);
 
   router.patch(
     '/:movieId',
     validationHandler({ movieId: movieIdSchema }, 'params'),
     validationHandler(updateMovieSchema),
-    async (req, res, next) => {
-      const { body: movieData } = req;
-      const { movieId } = req.params;
-
-      try {
-        const updatedMovie = await movieService.updateMovie(movieId, movieData);
-
-        res.status(200).send({
-          error: false,
-          message: 'Movie updated succesfully',
-          data: updatedMovie,
-        });
-      } catch (err) {
-        next(err);
-      }
-    }
+    updateMovie
   );
 
   router.delete(
     '/:movieId',
     validationHandler({ movieId: movieIdSchema }, 'params'),
-    async (req, res, next) => {
-      const { movieId } = req.params;
-
-      try {
-        await movieService.deleteMovie(movieId);
-
-        res.status(200).send({
-          error: false,
-          message: 'Movie deleted succesfully',
-          data: {},
-        });
-      } catch (err) {
-        next(err);
-      }
-    }
+    deleteMovie
   );
+}
+
+async function listMovies(req, res, next) {
+  const { genre } = req.query;
+
+  try {
+    const movies = await movieService.listMovies(genre);
+
+    res.status(200).send({
+      error: false,
+      message: 'Movies listed succesfully',
+      data: movies,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function createMovie(req, res, next) {
+  const { body: movie } = req;
+
+  try {
+    const createdMovie = await movieService.createMovie(movie);
+
+    res.status(201).send({
+      error: false,
+      message: 'Movie created succesfully',
+      data: createdMovie,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getMovie(req, res, next) {
+  const { movieId } = req.params;
+
+  try {
+    const movie = await movieService.getMovie(movieId);
+
+    res.status(200).send({
+      error: false,
+      message: 'Movie retrieved succesfully',
+      data: movie,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateMovie(req, res, next) {
+  const { body: movieData } = req;
+  const { movieId } = req.params;
+
+  try {
+    const updatedMovie = await movieService.updateMovie(movieId, movieData);
+
+    res.status(200).send({
+      error: false,
+      message: 'Movie updated succesfully',
+      data: updatedMovie,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteMovie(req, res, next) {
+  const { movieId } = req.params;
+
+  try {
+    await movieService.deleteMovie(movieId);
+
+    res.status(200).send({
+      error: false,
+      message: 'Movie deleted succesfully',
+      data: {},
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 module.exports = moviesApi;
